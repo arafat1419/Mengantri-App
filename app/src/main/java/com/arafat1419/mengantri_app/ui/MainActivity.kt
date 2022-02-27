07c096d2bb6093c395336ee52faa6ca893af0eec
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-02-27 15:49:02 +0800

    initial home in app

--- app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
+++ app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.arafat1419.mengantri_app
+package com.arafat1419.mengantri_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.arafat1419.mengantri_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
