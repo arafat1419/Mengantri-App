ab7aeb3e9b669c2ac01f243a358d72df6cec2e80
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-02-27 16:48:33 +0800

    add search in fragment home

--- app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
+++ app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
@@ -2,11 +2,18 @@ package com.arafat1419.mengantri_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 import com.arafat1419.mengantri_app.R
+import com.arafat1419.mengantri_app.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, HomeFragment())
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        }.commit()
     }
 }
