7b518ed552e992e1ce64024fdacb6c83eb0c740e
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-02-27 20:58:04 +0800

    finish interface fragment services

--- app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
+++ app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentTransaction
 import com.arafat1419.mengantri_app.R
 import com.arafat1419.mengantri_app.ui.companies.CompaniesFragment
 import com.arafat1419.mengantri_app.ui.home.HomeFragment
+import com.arafat1419.mengantri_app.ui.services.ServicesFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment_container, CompaniesFragment())
+            replace(R.id.fragment_container, ServicesFragment())
             setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
         }.commit()
     }
