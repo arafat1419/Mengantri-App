bb056e94e4d5ee2c99c33edd73e5aabddb08216a
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-01 19:58:16 +0800

    finish interface profile fragment

--- app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
+++ app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.arafat1419.mengantri_app.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.arafat1419.mengantri_app.R
-import com.arafat1419.mengantri_app.ui.companies.CompaniesFragment
 import com.arafat1419.mengantri_app.ui.detail.detailservice.DetailServiceFragment
-import com.arafat1419.mengantri_app.ui.detail.detailticket.DetailTicketFragment
-import com.arafat1419.mengantri_app.ui.home.HomeFragment
-import com.arafat1419.mengantri_app.ui.services.ServicesFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment_container, CompaniesFragment())
-            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-        }.commit()
+        val fragment = instantiateFragment()
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, fragment)
+                setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            }.commit()
+        }
+    }
+
+
+    private fun instantiateFragment(): Fragment? {
+        return try {
+            Class.forName("com.arafat1419.mengantri_app.profile.ui.ProfileFragment").newInstance() as Fragment
+        } catch (e: Exception) {
+            Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            null
+        }
     }
 }
