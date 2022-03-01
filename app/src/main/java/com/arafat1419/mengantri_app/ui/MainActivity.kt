0a3c3f6ec0475eec8101b90751f46197a310a548
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-01 12:29:42 +0800

    finish interface fragment detail ticket

--- app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
+++ app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentTransaction
 import com.arafat1419.mengantri_app.R
 import com.arafat1419.mengantri_app.ui.companies.CompaniesFragment
 import com.arafat1419.mengantri_app.ui.detail.detailservice.DetailServiceFragment
+import com.arafat1419.mengantri_app.ui.detail.detailticket.DetailTicketFragment
 import com.arafat1419.mengantri_app.ui.home.HomeFragment
 import com.arafat1419.mengantri_app.ui.services.ServicesFragment
 
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment_container, DetailServiceFragment())
+            replace(R.id.fragment_container, DetailTicketFragment())
             setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
         }.commit()
     }
