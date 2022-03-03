2f71e8e56056d5fbb5495e5c85b195e7b382a612
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-03 18:52:41 +0800

    create CategoryResponse.kt and add end point getCategories in ApiService.kt

--- core/src/main/java/com/arafat1419/mengantri_app/core/data/remote/api/ApiService.kt
+++ core/src/main/java/com/arafat1419/mengantri_app/core/data/remote/api/ApiService.kt
@@ -1,5 +1,6 @@
 package com.arafat1419.mengantri_app.core.data.remote.api
 
+import com.arafat1419.mengantri_app.core.data.remote.response.CategoryResponse
 import com.arafat1419.mengantri_app.core.data.remote.response.CustomerResponse
 import com.arafat1419.mengantri_app.core.data.remote.response.DataResponse
 import com.arafat1419.mengantri_app.core.data.remote.response.ListResponse
@@ -10,6 +11,7 @@ import retrofit2.http.Query
 
 interface ApiService {
 
+    // -- LOGIN MODULE --
     @GET("items/customer")
     suspend fun getLogin(
         @Query("filter[customer_status]") customerStatus: Int = 1,
@@ -20,4 +22,12 @@ interface ApiService {
     suspend fun postRegistration(
         @Body customerResponse: CustomerResponse
     ) : DataResponse<CustomerResponse>
+
+    // -- HOME MODULE --
+
+    @GET("/items/category")
+    suspend fun getCategories(
+        @Query("filter[category_status]") categoryStatus: Int = 1,
+        @Query("limit") limit: Int = 5
+    ) : ListResponse<CategoryResponse>
 }
---
+++ core/src/main/java/com/arafat1419/mengantri_app/core/data/remote/response/CategoryResponse.kt
@@ -0,0 +1,18 @@
+package com.arafat1419.mengantri_app.core.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class CategoryResponse(
+
+	@field:SerializedName("category_id")
+	val categoryId: Int? = null,
+
+	@field:SerializedName("category_name")
+	val categoryName: String? = null,
+
+	@field:SerializedName("category_image")
+	val categoryImage: String? = null,
+
+	@field:SerializedName("category_status")
+	val categoryStatus: Int? = null
+)
