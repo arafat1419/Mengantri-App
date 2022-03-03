d845c589e90fc3a3af2c98d5eb40e7778d452c06
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-03 17:21:34 +0800

    finish RegistrationFragment.kt

--- login/src/main/java/com/arafat1419/mengantri_app/login/ui/login/LoginFragment.kt
+++ login/src/main/java/com/arafat1419/mengantri_app/login/ui/login/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.arafat1419.mengantri_app.login.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- login/src/main/java/com/arafat1419/mengantri_app/login/ui/register/RegistrationFragment.kt
+++ login/src/main/java/com/arafat1419/mengantri_app/login/ui/register/RegistrationFragment.kt
@@ -4,16 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.arafat1419.mengantri_app.core.utils.CustomerSessionManager
 import com.arafat1419.mengantri_app.login.R
 import com.arafat1419.mengantri_app.login.databinding.FragmentRegistrationBinding
+import com.arafat1419.mengantri_app.login.di.loginModule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.context.loadKoinModules
 
+@FlowPreview
+@ExperimentalCoroutinesApi
 class RegistrationFragment : Fragment() {
 
     // Initilize binding with null because we need to set it null again when fragment destroy
     private var _binding: FragmentRegistrationBinding? = null
     private val binding get() = _binding
+
+    // Initialize viewModel with koin
+    private val viewModel: RegistrationViewModel by viewModel()
+
+    private lateinit var sessionManager: CustomerSessionManager
+
+    private var navHostFragment: Fragment? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +43,14 @@ class RegistrationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Load koin manually for multi modules
+        loadKoinModules(loginModule)
+
+        // Initialize session manager from customer session manager
+        sessionManager = CustomerSessionManager(requireContext())
+
         // Initialize nav host fragment as fragment container
-        val navHostFragment = parentFragmentManager.findFragmentById(R.id.login_container)
+        navHostFragment = parentFragmentManager.findFragmentById(R.id.login_container)
 
         // binding apply to reduce redundant code
         binding?.apply {
@@ -37,7 +59,67 @@ class RegistrationFragment : Fragment() {
                 navHostFragment?.findNavController()
                     ?.navigate(R.id.action_registrationFragment_to_loginFragment)
             }
+
+            btnRegistrationSignup.setOnClickListener {
+                // Check if all field has been filled
+                if (checkEditText()) {
+                    viewModel.postRegistration(
+                        edtRegistrationName.text.toString(),
+                        edtRegisrationEmail.text.toString(),
+                        edtRegistrationPassword.text.toString(),
+                        edtRegistrationPhone.text.toString()
+                    ).observe(viewLifecycleOwner, { customerDomain ->
+                        if (customerDomain.customerEmail == edtRegisrationEmail.text.toString()) {
+                            Toast.makeText(context, "Registration completed", Toast.LENGTH_SHORT)
+                                .show()
+                            sessionManager.clearCustomer()
+                            sessionManager.saveCustomer(customerDomain)
+                            navigateToLogin()
+                        } else {
+                            Toast.makeText(context, "Registration failed", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    })
+                }
+            }
         }
+    }
+
+    private fun navigateToLogin() {
+        // Navigate to Login
+        navHostFragment?.findNavController()
+            ?.navigate(R.id.action_registrationFragment_to_loginFragment)
+    }
+
+    private fun checkEditText(): Boolean {
+        var check = false
+        binding?.apply {
+            check = when {
+                edtRegisrationEmail.text?.isEmpty() == true -> {
+                    Toast.makeText(context, "Email cannot empty", Toast.LENGTH_SHORT).show()
+                    false
+                }
+                edtRegistrationPassword.text?.isEmpty() == true -> {
+                    Toast.makeText(context, "Password cannot empty", Toast.LENGTH_SHORT)
+                        .show()
+                    false
+                }
+                edtRegistrationName.text?.isEmpty() == true -> {
+                    Toast.makeText(context, "Name cannot empty", Toast.LENGTH_SHORT)
+                        .show()
+                    false
+                }
+                edtRegistrationPhone.text?.isEmpty() == true -> {
+                    Toast.makeText(context, "Phone cannot empty", Toast.LENGTH_SHORT)
+                        .show()
+                    false
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+        return check
     }
 
     override fun onDestroyView() {
