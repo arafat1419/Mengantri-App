581fcf0d0e77d88c7044131e49c0284d7846eb88
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-03 19:10:51 +0800

    initial di in home module

---
+++ home/src/main/java/com/arafat1419/mengantri_app/home/di/HomeModule.kt
@@ -0,0 +1,13 @@
+package com.arafat1419.mengantri_app.home.di
+
+import com.arafat1419.mengantri_app.home.ui.home.HomeViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+@ExperimentalCoroutinesApi
+@FlowPreview
+val homeModule = module {
+    viewModel { HomeViewModel(get()) }
+}
