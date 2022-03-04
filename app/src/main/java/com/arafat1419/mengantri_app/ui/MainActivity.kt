feb0b36d331392f0e29b40284f9f6ff7921f36b6
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-04 13:31:28 +0800

    remove unused import in MainActivity.kt

--- app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
+++ app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
@@ -2,11 +2,7 @@ package com.arafat1419.mengantri_app.ui
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.forEach
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -35,7 +31,8 @@ class MainActivity : AppCompatActivity() {
 
     // to setup bottom navigation bar
     private fun setupBottomNavBar() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
