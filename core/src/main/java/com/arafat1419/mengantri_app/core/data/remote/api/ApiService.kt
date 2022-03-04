451b992330666994c812579df012d5304f027f34
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-04 15:04:26 +0800

    create CompanyResponse.kt and add endpoint getCompanies() in ApiService.kt

--- core/src/main/java/com/arafat1419/mengantri_app/core/data/remote/api/ApiService.kt
+++ core/src/main/java/com/arafat1419/mengantri_app/core/data/remote/api/ApiService.kt
@@ -1,9 +1,6 @@
 package com.arafat1419.mengantri_app.core.data.remote.api
 
-import com.arafat1419.mengantri_app.core.data.remote.response.CategoryResponse
-import com.arafat1419.mengantri_app.core.data.remote.response.CustomerResponse
-import com.arafat1419.mengantri_app.core.data.remote.response.DataResponse
-import com.arafat1419.mengantri_app.core.data.remote.response.ListResponse
+import com.arafat1419.mengantri_app.core.data.remote.response.*
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -24,10 +21,15 @@ interface ApiService {
     ) : DataResponse<CustomerResponse>
 
     // -- HOME MODULE --
-
     @GET("/items/category")
     suspend fun getCategories(
         @Query("filter[category_status]") categoryStatus: Int = 1,
         @Query("limit") limit: Int = 5
     ) : ListResponse<CategoryResponse>
+
+    @GET("/items/companies")
+    suspend fun getCompanies(
+        @Query("filter[company_status]") companyStatus: Int = 1,
+        @Query("filter[category_id]") categoryId: Int
+    ) : ListResponse<CompanyResponse>
 }
---
+++ core/src/main/java/com/arafat1419/mengantri_app/core/data/remote/response/CompanyResponse.kt
@@ -0,0 +1,54 @@
+package com.arafat1419.mengantri_app.core.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class CompanyResponse(
+
+	@field:SerializedName("company_id")
+	val companyId: Int? = null,
+
+	@field:SerializedName("customer_id")
+	val customerId: Int? = null,
+
+	@field:SerializedName("company_name")
+	val companyName: String? = null,
+
+	@field:SerializedName("company_phone")
+	val companyPhone: String? = null,
+
+	@field:SerializedName("company_banner")
+	val companyBanner: String? = null,
+
+	@field:SerializedName("company_image")
+	val companyImage: String? = null,
+
+	@field:SerializedName("category_id")
+	val categoryId: Int? = null,
+
+	@field:SerializedName("company_address")
+	val companyAddress: String? = null,
+
+	@field:SerializedName("company_city")
+	val companyCity: String? = null,
+
+	@field:SerializedName("company_districs")
+	val companyDistrics: String? = null,
+
+	@field:SerializedName("company_status")
+	val companyStatus: Int? = null,
+
+	@field:SerializedName("company_date_created")
+	val companyDateCreated: String? = null,
+
+	@field:SerializedName("company_date_updated")
+	val companyDateUpdated: Any? = null,
+
+	@field:SerializedName("company_open_time")
+	val companyOpenTime: String? = null,
+
+	@field:SerializedName("company_close_time")
+	val companyCloseTime: String? = null,
+
+	@field:SerializedName("company_expired_time")
+	val companyExpiredTime: String? = null,
+)
