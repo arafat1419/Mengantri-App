830a3b4925f60e017f826c8592e1581953654457
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-08 15:02:26 +0800

    add logic code to show data in DetailTicketFragment.kt

--- home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailticket/DetailTicketFragment.kt
+++ home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailticket/DetailTicketFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import com.arafat1419.mengantri_app.core.domain.model.TicketWithServiceDomain
+import com.arafat1419.mengantri_app.core.utils.DateHelper
+import com.arafat1419.mengantri_app.assets.R
 import com.arafat1419.mengantri_app.home.databinding.FragmentDetailTicketBinding
 import com.arafat1419.mengantri_app.home.di.homeModule
-import com.arafat1419.mengantri_app.home.ui.detail.detailservice.DetailServiceViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -19,13 +22,12 @@ import org.koin.core.context.loadKoinModules
 @ExperimentalCoroutinesApi
 @FlowPreview
 class DetailTicketFragment : Fragment() {
-
     // Initilize binding with null because we need to set it null again when fragment destroy
     private var _binding: FragmentDetailTicketBinding? = null
     private val binding get() = _binding
 
     // Initialize viewModel with koin
-    private val viewModel: DetailServiceViewModel by viewModel()
+    private val viewModel: DetailTicketViewModel by viewModel()
 
     private var navHostFragment: Fragment? = null
 
@@ -47,12 +49,15 @@ class DetailTicketFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         _binding = FragmentDetailTicketBinding.inflate(layoutInflater, container, false)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val ticketId = arguments?.getInt(EXTRA_TICKET_ID)
 
         // Load koin manually for multi modules
         loadKoinModules(homeModule)
@@ -61,11 +66,92 @@ class DetailTicketFragment : Fragment() {
         navHostFragment =
             parentFragmentManager.findFragmentById(com.arafat1419.mengantri_app.R.id.fragment_container)
 
+        viewModel.getTicket(ticketId!!).observe(viewLifecycleOwner) { listTicketWithService ->
+            if (listTicketWithService.isNotEmpty()) {
+                showData(listTicketWithService[0])
+            } else {
+                Toast.makeText(context, "Empty ticket", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onResume() {
         super.onResume()
         (activity as AppCompatActivity?)?.supportActionBar?.hide()
+    }
+
+    private fun showData(data: TicketWithServiceDomain) {
+        binding?.apply {
+            if (data.serviceId != null) {
+                val serviceDomain = data.serviceId!!
+                txtServicesAppTitle.text = serviceDomain.serviceName
+                txtDTicketTitle.text = serviceDomain.serviceName
+                txtDTicketCompany.text = serviceDomain.companyId?.companyName
+            }
+            txtDTicketDay.text = DateHelper.toUpdateLabel(data.ticketDate!!)
+            txtDTicketTime.text = resources.getString(
+                R.string.time_format,
+                data.serviceId?.serviceOpenTime?.substring(0..4),
+                data.serviceId?.serviceCloseTime?.substring(0..4),
+            )
+            txtDTicketName.text = data.ticketPersonName
+            txtDTicketQueueId.text = data.ticketId.toString()
+            statusState(data)
+        }
+    }
+
+    private fun statusState(data: TicketWithServiceDomain) {
+        binding?.apply {
+            when (data.ticketStatus) {
+                "waiting" -> {
+                    statusNotWaiting(false)
+                    txtDTicketEst.text = data.ticketServiceTime
+                    viewModel.getServiceAndServed(data.serviceId?.companyId?.companyId!!)
+                        .observe(viewLifecycleOwner) { listCountDomain ->
+                            var waiting = 0
+                            listCountDomain.forEach { serviceCountDomain ->
+                                if (serviceCountDomain.services.serviceId == data.serviceId?.serviceId) {
+                                    waiting = serviceCountDomain.waiting!!
+                                }
+                            }
+
+                            txtDTicketQueueNumber.text = waiting.toString()
+                        }
+                }
+                "success" -> {
+                    statusNotWaiting(true)
+                    txtDTicketStatus.text = resources.getString(R.string.ticket_status_success)
+                }
+                "progress" -> {
+                    statusNotWaiting(true)
+                    txtDTicketStatus.text = resources.getString(R.string.ticket_status_progress)
+                }
+                "cancel" -> {
+                    statusNotWaiting(true)
+                    txtDTicketStatus.text = resources.getString(R.string.ticket_status_cancelled)
+                }
+            }
+        }
+    }
+
+    private fun statusNotWaiting(state: Boolean) {
+        binding?.apply {
+            if (state) {
+                txtDTicketStatus.visibility = View.VISIBLE
+
+                titleTicketQueueNumber.visibility = View.GONE
+                txtDTicketQueueNumber.visibility = View.GONE
+                titleTicketEst.visibility = View.GONE
+                txtDTicketEst.visibility = View.GONE
+            } else {
+                titleTicketQueueNumber.visibility = View.VISIBLE
+                txtDTicketQueueNumber.visibility = View.VISIBLE
+                titleTicketEst.visibility = View.VISIBLE
+                txtDTicketEst.visibility = View.VISIBLE
+
+                txtDTicketStatus.visibility = View.GONE
+            }
+        }
     }
 
     override fun onDestroyView() {
