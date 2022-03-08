71ff9729397206bda0b9069bb6f78d54138cf8f1
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-08 12:31:48 +0800

    add viewModel and additional need in DetailTicketFragment.kt

--- home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailticket/DetailTicketFragment.kt
+++ home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailticket/DetailTicketFragment.kt
@@ -4,38 +4,73 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.arafat1419.mengantri_app.home.R
+import androidx.navigation.fragment.NavHostFragment
+import com.arafat1419.mengantri_app.home.databinding.FragmentDetailTicketBinding
+import com.arafat1419.mengantri_app.home.di.homeModule
+import com.arafat1419.mengantri_app.home.ui.detail.detailservice.DetailServiceViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.context.loadKoinModules
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailTicketFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@ExperimentalCoroutinesApi
+@FlowPreview
 class DetailTicketFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    // Initilize binding with null because we need to set it null again when fragment destroy
+    private var _binding: FragmentDetailTicketBinding? = null
+    private val binding get() = _binding
+
+    // Initialize viewModel with koin
+    private val viewModel: DetailServiceViewModel by viewModel()
+
+    private var navHostFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        // This callback will only be called when MyFragment is at least Started.
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true /* enabled by default */) {
+                override fun handleOnBackPressed() {
+                    // Handle the back button event
+                    NavHostFragment.findNavController(this@DetailTicketFragment).navigateUp()
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_ticket, container, false)
+        _binding = FragmentDetailTicketBinding.inflate(layoutInflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Load koin manually for multi modules
+        loadKoinModules(homeModule)
+
+        // Initialize nav host fragment as fragment container
+        navHostFragment =
+            parentFragmentManager.findFragmentById(com.arafat1419.mengantri_app.R.id.fragment_container)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)?.supportActionBar?.hide()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
