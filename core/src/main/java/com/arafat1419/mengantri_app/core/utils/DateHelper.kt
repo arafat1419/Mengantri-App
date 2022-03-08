f8b4e55369ced43d040e4b8ba01ad40533fc57aa
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-08 13:17:47 +0800

    add toUpdateLabel fun in DateHelper.kt

--- core/src/main/java/com/arafat1419/mengantri_app/core/utils/DateHelper.kt
+++ core/src/main/java/com/arafat1419/mengantri_app/core/utils/DateHelper.kt
@@ -1,18 +1,31 @@
 package com.arafat1419.mengantri_app.core.utils
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 object DateHelper {
+    private const val formatShow = "EEEE, dd-MM-y"
+    private const val formatPost = "y-MM-dd"
+
     fun updateLabel(calendar: Calendar): String {
-        val myFormat = "EEEE, dd-MM-y"
-        val dateFormat = SimpleDateFormat(myFormat, Locale.getDefault())
+        val dateFormat = SimpleDateFormat(formatShow, Locale.getDefault())
         return dateFormat.format(calendar.time)
     }
 
     fun returnLabel(calendar: Calendar): String {
-        val myFormat = "y-MM-dd"
-        val dateFormat = SimpleDateFormat(myFormat, Locale.getDefault())
+        val dateFormat = SimpleDateFormat(formatPost, Locale.getDefault())
         return dateFormat.format(calendar.time)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun toUpdateLabel(dateString: String): String {
+        val date = LocalDate.parse(dateString, DateTimeFormatter.ISO_DATE)
+        val calendar = Calendar.getInstance()
+        calendar.set(date.year, date.monthValue, date.dayOfMonth)
+        return updateLabel(calendar)
     }
 }
