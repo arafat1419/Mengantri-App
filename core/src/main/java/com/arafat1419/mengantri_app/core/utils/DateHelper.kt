ac77652659a43d38f3ee27133e26d008e6674f84
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-08 15:49:36 +0800

    refactor DateHelper.kt

--- core/src/main/java/com/arafat1419/mengantri_app/core/utils/DateHelper.kt
+++ core/src/main/java/com/arafat1419/mengantri_app/core/utils/DateHelper.kt
@@ -18,18 +18,20 @@ object DateHelper {
     }
 
     fun toUpdateLabel(dateString: String): String {
-        val dateYear = dateString.substring(dateString.length - 4, dateString.length).toInt()
-        val dateMonth = dateString.substring(dateString.length - 7, dateString.length - 5).toInt()
-        val dateDay = dateString.substring(dateString.length - 10, dateString.length - 8).toInt()
+        val dateYear = dateString.substring(0..3).toInt()
+        val dateMonth = dateString.substring(5..6).toInt()
+        val dateDay = dateString.substring(8..9).toInt()
+
         val calendar = Calendar.getInstance()
         calendar.set(dateYear, dateMonth, dateDay)
         return updateLabel(calendar)
     }
 
     fun toReturnLabel(dateString: String): String {
-        val dateYear = dateString.substring(0..3).toInt()
-        val dateMonth = dateString.substring(5..6).toInt()
-        val dateDay = dateString.substring(8..9).toInt()
+        val dateYear = dateString.substring(dateString.length - 4, dateString.length).toInt()
+        val dateMonth = dateString.substring(dateString.length - 7, dateString.length - 5).toInt()
+        val dateDay = dateString.substring(dateString.length - 10, dateString.length - 8).toInt()
+
         val calendar = Calendar.getInstance()
         calendar.set(dateYear, dateMonth, dateDay)
         return updateLabel(calendar)
