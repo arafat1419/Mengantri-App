f0cf63aae22ef1dc194aea8255f27d61dc36a6fd
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-11 13:35:42 +0800

    refactor Registration Fragment and add navigation to regisVerifFragment

--- login/src/main/java/com/arafat1419/mengantri_app/login/ui/register/RegisVerificationFragment.kt
+++ login/src/main/java/com/arafat1419/mengantri_app/login/ui/register/RegisVerificationFragment.kt
@@ -39,22 +39,6 @@ class RegisVerificationFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisVerificationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisVerificationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val EXTRA_CUSTOMER_EMAIL = "extra_customer_email"
     }
 }
