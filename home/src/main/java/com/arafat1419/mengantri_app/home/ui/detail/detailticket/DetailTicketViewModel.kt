204575ea356536090e784b9b034860492ad84915
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-11 20:57:30 +0800

    change from getServiceAndServed to getTickets and waiting to queueNumber

--- home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailticket/DetailTicketViewModel.kt
+++ home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailticket/DetailTicketViewModel.kt
@@ -12,9 +12,9 @@ class DetailTicketViewModel(private val dataUseCase: DataUseCase) : ViewModel() 
     fun getTicket(ticketId: Int): LiveData<List<TicketWithServiceDomain>> =
         dataUseCase.getTicket(ticketId).asLiveData()
 
-    fun getServiceAndServed(companyId: Int): LiveData<List<ServiceCountDomain>> =
-        dataUseCase.getServicesAndServed(companyId).asLiveData()
-
     fun updateTicket(ticketId: Int, status: String): LiveData<TicketDomain> =
         dataUseCase.updateTicket(ticketId, status).asLiveData()
+
+    fun getTickets(serviceId: Int): LiveData<List<TicketDomain>> =
+        dataUseCase.getTickets(serviceId).asLiveData()
 }
