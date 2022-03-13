f8c52c37fe2707c18e1b42b0abaa70c76a03072e
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-13 11:48:10 +0800

    initial modal_edit_profil

--- profile/src/main/java/com/arafat1419/mengantri_app/profile/ui/ProfilePrefFragment.kt
+++ profile/src/main/java/com/arafat1419/mengantri_app/profile/ui/ProfilePrefFragment.kt
@@ -1,11 +1,12 @@
 package com.arafat1419.mengantri_app.profile.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.preference.PreferenceFragmentCompat
 import com.arafat1419.mengantri_app.profile.R
 
 class ProfilePrefFragment: PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        addPreferencesFromResource(R.xml.profile_preferences)
+        setPreferencesFromResource(R.xml.profile_preferences, rootKey)
     }
 }
