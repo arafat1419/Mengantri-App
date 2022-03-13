db63edf2e2ba6a1b2a5f71adae6ee0a210227e12
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-13 16:16:45 +0800

    unknown company file

---
+++ company/build/generated/source/buildConfig/debug/com/arafat1419/mengantri_app/company/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.arafat1419.mengantri_app.company;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.arafat1419.mengantri_app";
+  public static final String BUILD_TYPE = "debug";
+}
