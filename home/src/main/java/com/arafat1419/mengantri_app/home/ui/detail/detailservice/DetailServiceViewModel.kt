c580aad7288335099adbd719ea5aac6fa7ad63e1
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-13 16:18:37 +0800

    add getServiceXDay in DetailServiceViewModel

--- home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailservice/DetailServiceViewModel.kt
+++ home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailservice/DetailServiceViewModel.kt
@@ -3,6 +3,7 @@ package com.arafat1419.mengantri_app.home.ui.detail.detailservice
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.arafat1419.mengantri_app.core.domain.model.ServiceXDayDomain
 import com.arafat1419.mengantri_app.core.domain.model.TicketDomain
 import com.arafat1419.mengantri_app.core.domain.usecase.DataUseCase
 
@@ -26,4 +27,7 @@ class DetailServiceViewModel(private val dataUseCase: DataUseCase) : ViewModel()
             ticketNotes,
             ticketDate
         ).asLiveData()
+
+    fun getServiceXDay(serviceId: Int, dayId: Int): LiveData<List<ServiceXDayDomain>> =
+        dataUseCase.getServiceXDay(serviceId, dayId).asLiveData()
 }
