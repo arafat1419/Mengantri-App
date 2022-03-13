7b43109410379f24d47cf6327b0f2edd410d05f9
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-13 12:11:57 +0800

    add updateProfile in profileViewModel

--- profile/src/main/java/com/arafat1419/mengantri_app/profile/ui/ProfileViewModel.kt
+++ profile/src/main/java/com/arafat1419/mengantri_app/profile/ui/ProfileViewModel.kt
@@ -1,7 +1,16 @@
 package com.arafat1419.mengantri_app.profile.ui
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.arafat1419.mengantri_app.core.domain.model.CustomerDomain
 import com.arafat1419.mengantri_app.core.domain.usecase.DataUseCase
 
-class ProfileViewModel(private val dataUseCase: DataUseCase): ViewModel() {
+class ProfileViewModel(private val dataUseCase: DataUseCase) : ViewModel() {
+    fun updateProfile(
+        customerId: Int,
+        customerName: String,
+        customerPhone: String
+    ): LiveData<CustomerDomain> =
+        dataUseCase.updateProfile(customerId, customerName, customerPhone).asLiveData()
 }
