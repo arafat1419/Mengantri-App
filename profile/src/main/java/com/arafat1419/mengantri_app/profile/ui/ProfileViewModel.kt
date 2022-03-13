063b860c7c6ede21b94d9295c02178fb9cead079
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-13 12:10:05 +0800

    initialize profileViewModel and profileModule

---
+++ profile/src/main/java/com/arafat1419/mengantri_app/profile/ui/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.arafat1419.mengantri_app.profile.ui
+
+import androidx.lifecycle.ViewModel
+import com.arafat1419.mengantri_app.core.domain.usecase.DataUseCase
+
+class ProfileViewModel(private val dataUseCase: DataUseCase): ViewModel() {
+}
