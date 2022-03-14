604b451a6839844be4d6c3d3240c6ba0bddbcb7d
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-14 10:30:19 +0800

    add binding in RegistrationStatusActivity.kt

--- company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusActivity.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusActivity.kt
@@ -3,10 +3,14 @@ package com.arafat1419.mengantri_app.company.register
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.arafat1419.mengantri_app.company.R
+import com.arafat1419.mengantri_app.company.databinding.ActivityRegistrationStatusBinding
 
 class RegistrationStatusActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationStatusBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration_status)
+        binding = ActivityRegistrationStatusBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
