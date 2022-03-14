1e977a11aef7f2fc55d6c23d7d1336f38c7ba53f
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-14 11:02:08 +0800

    add statusHandler logic for status progress and expired in RegistrationStatusActivity.kt

--- company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusActivity.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusActivity.kt
@@ -2,13 +2,17 @@ package com.arafat1419.mengantri_app.company.register
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.arafat1419.mengantri_app.company.R
+import com.arafat1419.mengantri_app.R
 import com.arafat1419.mengantri_app.company.databinding.ActivityRegistrationStatusBinding
 import com.arafat1419.mengantri_app.company.di.companyModule
 import com.arafat1419.mengantri_app.core.utils.CustomerSessionManager
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
 
+@ExperimentalCoroutinesApi
+@FlowPreview
 class RegistrationStatusActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistrationStatusBinding
 
@@ -27,5 +31,43 @@ class RegistrationStatusActivity : AppCompatActivity() {
 
         // Initialize session manager from customer session manager
         sessionManager = CustomerSessionManager(this)
+
+        viewModel.getUserCompany(sessionManager.fetchCustomerId()).observe(this) { listCompany ->
+            if (listCompany.isNullOrEmpty()) {
+                statusHandler(4)
+            } else {
+                val status = listCompany[0].companyStatus!!
+                statusHandler(status)
+            }
+        }
+    }
+
+    private fun statusHandler(status: Int) {
+        when (status) {
+            0 -> {
+                binding.apply {
+                    imgRStatus.setImageResource(R.drawable.ic_progress_time)
+                    txtRStatus.text =
+                        getString(com.arafat1419.mengantri_app.company.R.string.registration_on_process)
+                    navigateToProfile()
+                }
+            }
+            1 -> "active"
+            2 -> {
+                binding.apply {
+                    imgRStatus.setImageResource(R.drawable.ic_expired_time)
+                    txtRStatus.text =
+                        getString(com.arafat1419.mengantri_app.company.R.string.registration_expired)
+                    navigateToProfile()
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun navigateToProfile() {
+        binding.btnRStatus.setOnClickListener { onBackPressed() }
     }
 }
