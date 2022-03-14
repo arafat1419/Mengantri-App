dfa679626065def85271bf2c28be99ad77ccdef8
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-14 09:47:48 +0800

    initial registerStatusActivity

---
+++ company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusActivity.kt
@@ -0,0 +1,12 @@
+package com.arafat1419.mengantri_app.company.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.arafat1419.mengantri_app.company.R
+
+class RegistrationStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_status)
+    }
+}
