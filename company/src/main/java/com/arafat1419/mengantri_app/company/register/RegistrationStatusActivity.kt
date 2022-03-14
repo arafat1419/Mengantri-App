5778975cd225d002a5ee611ff770b06817c6d2d4
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-14 11:37:29 +0800

    move logic code in activityRegStatus to fragmentRegStatus

--- company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusActivity.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusActivity.kt
@@ -11,63 +11,14 @@ import kotlinx.coroutines.FlowPreview
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
 
-@ExperimentalCoroutinesApi
-@FlowPreview
 class RegistrationStatusActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistrationStatusBinding
-
-    // Initialize viewModel with koin
-    private val viewModel: RegisterStatusViewModel by viewModel()
-
-    private lateinit var sessionManager: CustomerSessionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationStatusBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Load koin manually for multi modules
-        loadKoinModules(companyModule)
-
-        // Initialize session manager from customer session manager
-        sessionManager = CustomerSessionManager(this)
-
-        viewModel.getUserCompany(sessionManager.fetchCustomerId()).observe(this) { listCompany ->
-            if (listCompany.isNullOrEmpty()) {
-                statusHandler(4)
-            } else {
-                val status = listCompany[0].companyStatus!!
-                statusHandler(status)
-            }
-        }
     }
 
-    private fun statusHandler(status: Int) {
-        when (status) {
-            0 -> {
-                binding.apply {
-                    imgRStatus.setImageResource(R.drawable.ic_progress_time)
-                    txtRStatus.text =
-                        getString(com.arafat1419.mengantri_app.company.R.string.registration_on_process)
-                    navigateToProfile()
-                }
-            }
-            1 -> "active"
-            2 -> {
-                binding.apply {
-                    imgRStatus.setImageResource(R.drawable.ic_expired_time)
-                    txtRStatus.text =
-                        getString(com.arafat1419.mengantri_app.company.R.string.registration_expired)
-                    navigateToProfile()
-                }
-            }
-            else -> {
 
-            }
-        }
-    }
-
-    private fun navigateToProfile() {
-        binding.btnRStatus.setOnClickListener { onBackPressed() }
-    }
 }
--- company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusFragment.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/register/RegistrationStatusFragment.kt
@@ -1,60 +1,86 @@
 package com.arafat1419.mengantri_app.company.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.arafat1419.mengantri_app.company.R
+import androidx.fragment.app.Fragment
+import com.arafat1419.mengantri_app.company.databinding.FragmentRegistrationStatusBinding
+import com.arafat1419.mengantri_app.company.di.companyModule
+import com.arafat1419.mengantri_app.core.utils.CustomerSessionManager
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.context.loadKoinModules
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegistrationStatusFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@ExperimentalCoroutinesApi
+@FlowPreview
 class RegistrationStatusFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Initilize binding with null because we need to set it null again when fragment destroy
+    private var _binding: FragmentRegistrationStatusBinding? = null
+    private val binding get() = _binding
+
+    // Initialize viewModel with koin
+    private val viewModel: RegisterStatusViewModel by viewModel()
+
+    private lateinit var sessionManager: CustomerSessionManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_status, container, false)
+        _binding = FragmentRegistrationStatusBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistrationStatusFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistrationStatusFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Load koin manually for multi modules
+        loadKoinModules(companyModule)
+
+        // Initialize session manager from customer session manager
+        sessionManager = CustomerSessionManager(requireContext())
+
+        viewModel.getUserCompany(sessionManager.fetchCustomerId())
+            .observe(viewLifecycleOwner) { listCompany ->
+                if (listCompany.isNullOrEmpty()) {
+                    statusHandler(4)
+                } else {
+                    val status = listCompany[0].companyStatus!!
+                    statusHandler(status)
                 }
             }
+    }
+
+    private fun statusHandler(status: Int) {
+        when (status) {
+            0 -> {
+                binding?.apply {
+                    imgRStatus.setImageResource(com.arafat1419.mengantri_app.R.drawable.ic_progress_time)
+                    txtRStatus.text =
+                        getString(com.arafat1419.mengantri_app.company.R.string.registration_on_process)
+                    navigateToProfile()
+                }
+            }
+            1 -> "active"
+            2 -> {
+                binding?.apply {
+                    imgRStatus.setImageResource(com.arafat1419.mengantri_app.R.drawable.ic_expired_time)
+                    txtRStatus.text =
+                        getString(com.arafat1419.mengantri_app.company.R.string.registration_expired)
+                    navigateToProfile()
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun navigateToProfile() {
+        binding?.btnRStatus?.setOnClickListener { activity?.onBackPressed() }
     }
 }
