36078374f0f8036e0d0b6f8b3c2b778234ca18b6
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-14 10:43:51 +0800

    add di and RegisterStatusViewModel in company module

---
+++ company/src/main/java/com/arafat1419/mengantri_app/company/register/RegisterStatusViewModel.kt
@@ -0,0 +1,8 @@
+package com.arafat1419.mengantri_app.company.register
+
+import androidx.lifecycle.ViewModel
+import com.arafat1419.mengantri_app.core.domain.usecase.DataUseCase
+
+class RegisterStatusViewModel(private val dataUseCase: DataUseCase): ViewModel() {
+
+}
