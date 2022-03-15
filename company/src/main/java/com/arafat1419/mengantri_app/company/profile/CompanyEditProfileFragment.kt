4334bbcc24604da719ee35fa3546be2a5244bf5b
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-15 12:57:32 +0800

    add ViewModel in CompanyEditProfileFragment.kt

--- company/src/main/java/com/arafat1419/mengantri_app/company/profile/CompanyEditProfileFragment.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/profile/CompanyEditProfileFragment.kt
@@ -8,12 +8,23 @@ import android.view.ViewGroup
 import com.arafat1419.mengantri_app.company.R
 import com.arafat1419.mengantri_app.company.databinding.FragmentCompanyEditProfileBinding
 import com.arafat1419.mengantri_app.company.databinding.FragmentRegistrationStatusBinding
+import com.arafat1419.mengantri_app.company.di.companyModule
+import com.arafat1419.mengantri_app.company.register.RegisterStatusViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.context.loadKoinModules
 
+@ExperimentalCoroutinesApi
+@FlowPreview
 class CompanyEditProfileFragment : Fragment() {
 
     // Initilize binding with null because we need to set it null again when fragment destroy
     private var _binding: FragmentCompanyEditProfileBinding? = null
     private val binding get() = _binding
+
+    // Initialize viewModel with koin
+    private val viewModel: CompanyProfileViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +33,13 @@ class CompanyEditProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentCompanyEditProfileBinding.inflate(layoutInflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Load koin manually for multi modules
+        loadKoinModules(companyModule)
     }
 
     override fun onDestroyView() {
