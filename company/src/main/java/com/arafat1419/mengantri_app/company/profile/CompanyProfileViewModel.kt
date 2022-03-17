44a7d222435ed357b706904be630daa1bfe8405f
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-03-17 13:36:57 +0800

    add postUploadFile in CompanyProfileViewModel.kt

--- company/src/main/java/com/arafat1419/mengantri_app/company/profile/CompanyProfileViewModel.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/profile/CompanyProfileViewModel.kt
@@ -4,12 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.arafat1419.mengantri_app.core.domain.model.CategoryDomain
+import com.arafat1419.mengantri_app.core.domain.model.UploadFileDomain
 import com.arafat1419.mengantri_app.core.domain.model.provincedomain.CityDomain
 import com.arafat1419.mengantri_app.core.domain.model.provincedomain.DistricsDomain
 import com.arafat1419.mengantri_app.core.domain.model.provincedomain.ProvinceDomain
 import com.arafat1419.mengantri_app.core.domain.usecase.DataUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import java.io.File
 
-class CompanyProfileViewModel(private val dataUseCase: DataUseCase): ViewModel() {
+@FlowPreview
+@ExperimentalCoroutinesApi
+class CompanyProfileViewModel(private val dataUseCase: DataUseCase) : ViewModel() {
     fun getProvinces(): LiveData<List<ProvinceDomain>> =
         dataUseCase.getProvinces().asLiveData()
 
@@ -21,4 +27,11 @@ class CompanyProfileViewModel(private val dataUseCase: DataUseCase): ViewModel()
 
     fun getCategories(): LiveData<List<CategoryDomain>> =
         dataUseCase.getCategories().asLiveData()
+
+    fun postUploadFile(
+        fileName: String,
+        isBanner: Boolean,
+        file: File
+    ): LiveData<UploadFileDomain> =
+        dataUseCase.postUploadFile(fileName, isBanner, file).asLiveData()
 }
