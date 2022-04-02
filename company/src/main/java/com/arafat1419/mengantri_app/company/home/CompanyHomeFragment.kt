76ed9123a1c1a3b07036d2d9a97dc485363348a3
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-04-02 16:34:54 +0800

    add getServiceAndServed and display data using service adapter

--- company/src/main/java/com/arafat1419/mengantri_app/company/home/CompanyHomeFragment.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/home/CompanyHomeFragment.kt
@@ -1,13 +1,20 @@
 package com.arafat1419.mengantri_app.company.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.arafat1419.mengantri_app.company.R
 import com.arafat1419.mengantri_app.company.databinding.FragmentCompanyHomeBinding
 import com.arafat1419.mengantri_app.company.di.companyModule
+import com.arafat1419.mengantri_app.core.domain.model.ServiceCountDomain
+import com.arafat1419.mengantri_app.core.ui.AdapterCallback
+import com.arafat1419.mengantri_app.core.ui.adapter.ServicesAdapter
+import com.arafat1419.mengantri_app.core.utils.CompanySessionManager
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -15,11 +22,13 @@ import org.koin.core.context.loadKoinModules
 
 @ExperimentalCoroutinesApi
 @FlowPreview
-class CompanyHomeFragment : Fragment() {
+class CompanyHomeFragment : Fragment(), AdapterCallback<ServiceCountDomain> {
 
     // Initilize binding with null because we need to set it null again when fragment destroy
     private var _binding: FragmentCompanyHomeBinding? = null
     private val binding get() = _binding
+
+    private lateinit var companySessionManager: CompanySessionManager
 
     // Initialize viewModel with koin
     private val viewModel: CompanyHomeViewModel by viewModel()
@@ -39,6 +48,32 @@ class CompanyHomeFragment : Fragment() {
         // Load koin manually for multi modules
         loadKoinModules(companyModule)
 
+        setRecyclerView()
+
+        companySessionManager = CompanySessionManager(requireContext())
+
+        viewModel.getServiceAndServed(companySessionManager.fetchCompanyId()).observe(viewLifecycleOwner) {
+            binding?.rvCompanyServices?.adapter.let { adapter ->
+                when (adapter) {
+                    is ServicesAdapter -> {
+                        adapter.setData(it)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onItemClicked(data: ServiceCountDomain) {
+        Toast.makeText(context, "Later", Toast.LENGTH_SHORT).show()
+    }
+
+    // Set recycler view with grid and use companies adapter as adapter
+    private fun setRecyclerView() {
+        binding?.rvCompanyServices?.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = ServicesAdapter(this@CompanyHomeFragment)
+        }
     }
 
     override fun onDestroyView() {
