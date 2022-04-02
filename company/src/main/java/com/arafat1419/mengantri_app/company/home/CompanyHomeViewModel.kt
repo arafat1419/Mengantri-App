4ad4b1f3ee1206e300d013447fb78bfd97b09e1e
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-04-02 15:34:56 +0800

    change getService to getServiceAndServed in companyHomeViewModel

--- company/src/main/java/com/arafat1419/mengantri_app/company/home/CompanyHomeViewModel.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/home/CompanyHomeViewModel.kt
@@ -3,10 +3,11 @@ package com.arafat1419.mengantri_app.company.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.arafat1419.mengantri_app.core.domain.model.ServiceCountDomain
 import com.arafat1419.mengantri_app.core.domain.model.ServiceDomain
 import com.arafat1419.mengantri_app.core.domain.usecase.DataUseCase
 
 class CompanyHomeViewModel(private val dataUseCase: DataUseCase) : ViewModel() {
-    fun getServices(companyId: Int): LiveData<List<ServiceDomain>> =
-        dataUseCase.getServices(companyId).asLiveData()
+    fun getServiceAndServed(companyId: Int): LiveData<List<ServiceCountDomain>> =
+        dataUseCase.getServicesAndServed(companyId).asLiveData()
 }
