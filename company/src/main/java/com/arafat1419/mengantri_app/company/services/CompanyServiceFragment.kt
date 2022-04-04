649d5670c899621b13042bd5b2a74f457b5b64ce
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-04-04 16:11:22 +0800

    add getServiceAndServed and display data using service adapter

--- company/src/main/java/com/arafat1419/mengantri_app/company/services/CompanyServiceFragment.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/services/CompanyServiceFragment.kt
@@ -1,60 +1,83 @@
 package com.arafat1419.mengantri_app.company.services
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.arafat1419.mengantri_app.company.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.arafat1419.mengantri_app.company.databinding.FragmentCompanyServiceBinding
+import com.arafat1419.mengantri_app.company.di.companyModule
+import com.arafat1419.mengantri_app.core.domain.model.ServiceCountDomain
+import com.arafat1419.mengantri_app.core.domain.model.ServiceDomain
+import com.arafat1419.mengantri_app.core.ui.AdapterCallback
+import com.arafat1419.mengantri_app.core.ui.adapter.CompanyServicesAdapter
+import com.arafat1419.mengantri_app.core.ui.adapter.ServicesAdapter
+import com.arafat1419.mengantri_app.core.utils.CompanySessionManager
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.context.loadKoinModules
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@ExperimentalCoroutinesApi
+@FlowPreview
+class CompanyServiceFragment : Fragment(), AdapterCallback<ServiceDomain> {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CompanyServiceFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CompanyServiceFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    // Initilize binding with null because we need to set it null again when fragment destroy
+    private var _binding: FragmentCompanyServiceBinding? = null
+    private val binding get() = _binding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var companySessionManager: CompanySessionManager
+
+    // Initialize viewModel with koin
+    private val viewModel: CompanyServicesViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_company_service, container, false)
+        _binding = FragmentCompanyServiceBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CompanyServiceFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CompanyServiceFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Load koin manually for multi modules
+        loadKoinModules(companyModule)
+
+        setRecyclerView()
+
+        companySessionManager = CompanySessionManager(requireContext())
+
+        viewModel.getServices(companySessionManager.fetchCompanyId()).observe(viewLifecycleOwner) {
+            binding?.rvEditServices?.adapter.let { adapter ->
+                when (adapter) {
+                    is CompanyServicesAdapter -> {
+                        adapter.setData(it)
+                        adapter.notifyDataSetChanged()
+                    }
                 }
             }
+        }
+    }
+
+    override fun onItemClicked(data: ServiceDomain) {
+        Toast.makeText(context, "Later", Toast.LENGTH_SHORT).show()
+    }
+
+    // Set recycler view with grid and use companies adapter as adapter
+    private fun setRecyclerView() {
+        binding?.rvEditServices?.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = CompanyServicesAdapter(this@CompanyServiceFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
