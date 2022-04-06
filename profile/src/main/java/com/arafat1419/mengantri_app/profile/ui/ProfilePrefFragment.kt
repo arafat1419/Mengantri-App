9b1154b8f91893dc3b31098737035416bf0ba23f
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-04-06 16:18:36 +0800

    close mainActivity when intent to companyActivity

--- profile/src/main/java/com/arafat1419/mengantri_app/profile/ui/ProfilePrefFragment.kt
+++ profile/src/main/java/com/arafat1419/mengantri_app/profile/ui/ProfilePrefFragment.kt
@@ -208,6 +208,7 @@ class ProfilePrefFragment : PreferenceFragmentCompat() {
                 Class.forName("com.arafat1419.mengantri_app.company.register.RegistrationStatusActivity")
             ).also {
                 startActivity(it)
+                activity?.finish()
             }
         } catch (e: Exception) {
             Toast.makeText(context, "Module not found", Toast.LENGTH_SHORT).show()
