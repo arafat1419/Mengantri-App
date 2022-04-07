a31f9041e9fa2bf859550bdf92519d382a43768c
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-04-07 21:25:49 +0800

    create CompanyCustomersAdapter.kt

---
+++ core/src/main/java/com/arafat1419/mengantri_app/core/ui/adapter/CompanyCustomersAdapter.kt
@@ -0,0 +1,68 @@
+package com.arafat1419.mengantri_app.core.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.arafat1419.mengantri_app.assets.R
+import com.arafat1419.mengantri_app.core.databinding.ListCustomersBinding
+import com.arafat1419.mengantri_app.core.domain.model.TicketDomain
+import com.arafat1419.mengantri_app.core.domain.model.TicketWithServiceDomain
+import com.arafat1419.mengantri_app.core.ui.AdapterCallback
+import com.arafat1419.mengantri_app.core.utils.StatusHelper
+
+class CompanyCustomersAdapter(private val callback: AdapterCallback<TicketWithServiceDomain>) :
+    RecyclerView.Adapter<CompanyCustomersAdapter.ViewHolder>() {
+    private var listData = ArrayList<TicketWithServiceDomain>()
+
+    fun setData(newListData: List<TicketWithServiceDomain>?) {
+        if (newListData == null) return
+        listData.clear()
+        listData.addAll(newListData)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CompanyCustomersAdapter.ViewHolder {
+        val itemsDataBinding =
+            ListCustomersBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(itemsDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: CompanyCustomersAdapter.ViewHolder, position: Int) {
+        val data = listData[position]
+        holder.bind(data)
+    }
+
+    override fun getItemCount(): Int = listData.size
+
+    inner class ViewHolder(private val binding: ListCustomersBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: TicketWithServiceDomain) {
+            with(binding) {
+                txtTicketId.text = data.ticketId.toString()
+                val timeFormat = itemView.resources.getString(R.string.time_format)
+                txtTicketStatus.text = data.ticketStatus
+                when (data.ticketStatus) {
+                    StatusHelper.TICKET_PROGRESS -> cardLTicketStatus.setBackgroundColor(
+                        ContextCompat.getColor(itemView.context, R.color.primary)
+                    )
+                    StatusHelper.TICKET_WAITING -> cardLTicketStatus.setBackgroundColor(
+                        ContextCompat.getColor(itemView.context, R.color.c_yellow)
+                    )
+                    StatusHelper.TICKET_CANCEL -> cardLTicketStatus.setBackgroundColor(
+                        ContextCompat.getColor(itemView.context, R.color.c_red)
+                    )
+                    StatusHelper.TICKET_SUCCESS -> cardLTicketStatus.setBackgroundColor(
+                        ContextCompat.getColor(itemView.context, R.color.c_green)
+                    )
+                }
+                itemView.setOnClickListener {
+                    callback.onItemClicked(data)
+                }
+            }
+        }
+    }
+}
