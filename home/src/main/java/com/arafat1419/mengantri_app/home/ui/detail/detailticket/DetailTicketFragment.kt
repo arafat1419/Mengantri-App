745376a38e0b31f1d2589bdaa44af05025396287
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-05-30 20:46:20 +0800

    create update ticket for progress and success

--- home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailticket/DetailTicketFragment.kt
+++ home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailticket/DetailTicketFragment.kt
@@ -1,7 +1,6 @@
 package com.arafat1419.mengantri_app.home.ui.detail.detailticket
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -91,7 +90,33 @@ class DetailTicketFragment : Fragment() {
 
         binding?.apply {
             btnDTicketCancel.setOnClickListener {
-                ticketId?.let { it1 -> showDialogAlert(it1) }
+                showDialogAlert(
+                    ticketId,
+                    StatusHelper.TICKET_CANCEL,
+                    "Are you sure to cancel this ticket?",
+                    resources.getString(R.string.ticket_status_cancelled)
+                )
+            }
+
+            btnDTicket.setOnClickListener {
+                when (btnDTicket.text) {
+                    resources.getString(R.string.done) -> {
+                        showDialogAlert(
+                            ticketId,
+                            StatusHelper.TICKET_SUCCESS,
+                            "Are you sure to finish this ticket?",
+                            resources.getString(R.string.ticket_status_success)
+                        )
+                    }
+                    resources.getString(R.string.process) -> {
+                        showDialogAlert(
+                            ticketId,
+                            StatusHelper.TICKET_PROGRESS,
+                            "Are you sure to process this ticket?",
+                            resources.getString(R.string.ticket_status_progress)
+                        )
+                    }
+                }
             }
         }
 
@@ -226,21 +251,26 @@ class DetailTicketFragment : Fragment() {
         }
     }
 
-    private fun showDialogAlert(ticketId: Int) {
+    private fun showDialogAlert(
+        ticketId: Int?,
+        newStatus: String,
+        message: String,
+        ticketStatusMsg: String
+    ) {
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle("Cancel Ticket?")
-        builder.setMessage(R.string.ticket_cancel_message)
+        builder.setMessage(message)
             .setPositiveButton(R.string.yes) { _, _ ->
-                Log.d("Lihat Dialog", "Yes")
-                viewModel.updateTicket(ticketId, StatusHelper.TICKET_CANCEL)
-                    .observe(viewLifecycleOwner) { ticketDomain ->
-                        Log.d("Lihat detail dialog", ticketDomain.toString())
-                        if (ticketDomain != null) {
-                            Toast.makeText(context, "Ticket has been cancelled", Toast.LENGTH_SHORT)
-                                .show()
-                            backToHome()
+                ticketId?.let {
+                    viewModel.updateTicket(it, newStatus)
+                        .observe(viewLifecycleOwner) { ticketDomain ->
+                            if (ticketDomain != null) {
+                                Toast.makeText(context, ticketStatusMsg, Toast.LENGTH_SHORT)
+                                    .show()
+                                backToHome()
+                            }
                         }
-                    }
+                }
             }
             .setNegativeButton(R.string.no) { dialog, _ ->
                 dialog.cancel()
