29d9c419da44c348ccaf5a7530864961a1087f2d
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-07-04 17:35:51 +0800

    re-implement fragment_companies.xml

--- app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
+++ app/src/main/java/com/arafat1419/mengantri_app/ui/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.arafat1419.mengantri_app.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.arafat1419.mengantri_app.R
+import com.arafat1419.mengantri_app.core.utils.LanguageSessionManager
 import com.arafat1419.mengantri_app.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,16 +37,43 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
+        navController.addOnDestinationChangedListener { controller, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> {
+                    showToolbar(true)
+                }
+                R.id.ticketsFragment -> {
+                    showToolbar(true)
+                }
+                R.id.profileFragment -> {
+                    showToolbar(true)
+                }
+                else -> {
+                    actionBar?.hide()
+                    showToolbar(false)
+                }
 
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.ticketsFragment,
-                R.id.profileFragment
-            ),
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.mainBottomNavigation.setupWithNavController(navController)
+            }
 
+            val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.homeFragment,
+                    R.id.ticketsFragment,
+                    R.id.profileFragment
+                ),
+            )
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            binding.mainBottomNavigation.setupWithNavController(navController)
+        }
+    }
+
+    private fun showToolbar(status: Boolean) {
+        binding.apply {
+            if (status) {
+                materialToolbar.visibility = View.VISIBLE
+            } else {
+                materialToolbar.visibility = View.GONE
+            }
+        }
     }
 }
