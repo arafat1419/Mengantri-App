df57ccc6f8422d34b72a27f7373378af128f00ca
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-07-30 01:07:08 +0800

    re-implement HomeFragment.kt

--- home/src/main/java/com/arafat1419/mengantri_app/home/ui/home/HomeFragment.kt
+++ home/src/main/java/com/arafat1419/mengantri_app/home/ui/home/HomeFragment.kt
@@ -9,42 +9,55 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.arafat1419.mengantri_app.R
-import com.arafat1419.mengantri_app.core.domain.model.CategoryDomain
-import com.arafat1419.mengantri_app.core.ui.AdapterCallback
 import com.arafat1419.mengantri_app.core.ui.adapter.CategoriesAdapter
+import com.arafat1419.mengantri_app.core.ui.adapter.CompaniesAdapter
+import com.arafat1419.mengantri_app.core.utils.CustomerSessionManager
 import com.arafat1419.mengantri_app.core.utils.StatusHelper.EXTRA_FRAGMENT_STATUS
 import com.arafat1419.mengantri_app.core.utils.StatusHelper.EXTRA_TICKET_ID
 import com.arafat1419.mengantri_app.home.databinding.FragmentHomeBinding
 import com.arafat1419.mengantri_app.home.di.homeModule
 import com.arafat1419.mengantri_app.home.ui.companies.CompaniesFragment
 import com.arafat1419.mengantri_app.home.ui.detail.detailticket.DetailTicketFragment
+import com.arafat1419.mengantri_app.home.ui.services.ServicesFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import es.dmoral.toasty.Toasty
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
 
 
+@ObsoleteCoroutinesApi
 @ExperimentalCoroutinesApi
 @FlowPreview
-class HomeFragment : Fragment(), AdapterCallback<CategoryDomain> {
+class HomeFragment : Fragment() {
 
     // Initilize binding with null because we need to set it null again when fragment destroy
     private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding
+    private val binding get() = _binding!!
 
     // Initialize viewModel with koin
     private val viewModel: HomeViewModel by viewModel()
 
     private var navHostFragment: Fragment? = null
 
+    private val categoriesAdapter: CategoriesAdapter by lazy { CategoriesAdapter() }
+    private val newestAdapter: CompaniesAdapter by lazy { CompaniesAdapter() }
+
+    private val customerSessionManager: CustomerSessionManager by lazy {
+        CustomerSessionManager(
+            requireContext()
+        )
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
-        return binding?.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -66,10 +79,10 @@ class HomeFragment : Fragment(), AdapterCallback<CategoryDomain> {
                 val bottomNavigationView =
                     requireActivity().findViewById<BottomNavigationView>(R.id.main_bottom_navigation)
                 bottomNavigationView.visibility = View.GONE
+
                 val bundle = bundleOf(
                     DetailTicketFragment.EXTRA_TICKET_ID to ticketIdFromOtherModule,
                     DetailTicketFragment.EXTRA_FROM_OTHER to isFromOtherModule
-
                 )
                 navHostFragment?.findNavController()?.navigate(
                     R.id.action_homeFragment_to_detailTicketFragment,
@@ -78,39 +91,88 @@ class HomeFragment : Fragment(), AdapterCallback<CategoryDomain> {
             }
         }
 
-        // get categories from view model in set the data to categories adapter
+        getCategories()
+        getNewestCompanies()
+        checkCustomerCompany()
+        onItemClicked()
+
+    }
+
+    // get categories from view model in set the data to categories adapter
+    private fun getCategories() {
         viewModel.getCategories().observe(viewLifecycleOwner) { listCategories ->
-            binding?.rvCategory?.adapter.let { adapter ->
-                when (adapter) {
-                    is CategoriesAdapter -> {
-                        adapter.setData(listCategories)
-                        adapter.notifyDataSetChanged()
+            if (listCategories.isNullOrEmpty()) {
+                categoriesAdapter.setData(listCategories)
+                categoriesAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    // get newest companies from view model in set the data to newest adapter
+    private fun getNewestCompanies() {
+        viewModel.getNewestComapanies().observe(viewLifecycleOwner) { listNewestCompanies ->
+            if (listNewestCompanies.isNullOrEmpty()) {
+                newestAdapter.setData(listNewestCompanies)
+                newestAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    private fun checkCustomerCompany() {
+        val customerId = customerSessionManager.fetchCustomerId()
+        viewModel.getCustomerCompany(customerId).observe(viewLifecycleOwner) { listCompany ->
+            binding.apply {
+                if (listCompany.isNullOrEmpty()) {
+                    // TODO : Add intent to companyRegistration
+                } else {
+                    when (listCompany[0].companyStatus) {
+                        0 -> Toasty.error(requireContext(), getString(com.arafat1419.mengantri_app.assets.R.string.registration_on_process))
+                        2 -> Toasty.error(requireContext(), getString(com.arafat1419.mengantri_app.assets.R.string.registration_expired))
+                        else -> cardJoin.visibility = View.GONE
                     }
                 }
             }
+
+        }
+    }
+
+    // move to companies fragment with category domain
+    private fun onItemClicked() {
+        categoriesAdapter.onItemClicked = {
+            val bundle = bundleOf(
+                CompaniesFragment.EXTRA_COMPANY_ID to it.categoryId
+            )
+            navHostFragment?.findNavController()
+                ?.navigate(R.id.action_homeFragment_to_companiesFragment, bundle)
         }
 
-        binding?.edtHomeSearch?.setOnClickListener {
+        newestAdapter.onItemClicked = {
+            val bundle = bundleOf(
+                ServicesFragment.EXTRA_COMPANY_DOMAIN to it
+            )
+            navHostFragment?.findNavController()?.navigate(
+                R.id.action_homeFragment_to_servicesFragment, bundle
+            )
+        }
+
+        binding.edtHomeSearch.setOnClickListener {
             navHostFragment?.findNavController()?.navigate(
                 R.id.action_homeFragment_to_searchFragment
             )
         }
     }
 
-    // move to companies fragment with category domain
-    override fun onItemClicked(data: CategoryDomain) {
-        val bundle = bundleOf(
-            CompaniesFragment.EXTRA_COMPANY_ID to data.categoryId
-        )
-        navHostFragment?.findNavController()
-            ?.navigate(R.id.action_homeFragment_to_companiesFragment, bundle)
-    }
-
     // Set recycler view with grid and use categories adapter as adapter
     private fun setRecyclerView() {
-        binding?.rvCategory?.apply {
-            layoutManager = GridLayoutManager(context, 3)
-            adapter = CategoriesAdapter(this@HomeFragment)
+        binding.apply {
+            rvCategory.apply {
+                layoutManager = GridLayoutManager(context, 3)
+                adapter = categoriesAdapter
+            }
+            rvNewest.apply {
+                layoutManager = GridLayoutManager(context, 2)
+                adapter = newestAdapter
+            }
         }
     }
 
