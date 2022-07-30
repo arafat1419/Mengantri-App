a56dc5f5eb1866d2ce47d97479868b883bad6ac4
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-07-30 14:05:02 +0800

    add end-point function at TicketsViewModel.kt

--- ticket/src/main/java/com/arafat1419/mengantri_app/ticket/ui/TicketsViewModel.kt
+++ ticket/src/main/java/com/arafat1419/mengantri_app/ticket/ui/TicketsViewModel.kt
@@ -1,15 +1,10 @@
 package com.arafat1419.mengantri_app.ticket.ui
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.arafat1419.mengantri_app.core.domain.model.TicketWithServiceDomain
 import com.arafat1419.mengantri_app.core.domain.usecase.DataUseCase
 
 class TicketsViewModel(private val dataUseCase: DataUseCase) : ViewModel() {
-    fun getTicketByStatus(
-        customerId: Int,
-        ticketStatus: String
-    ): LiveData<List<TicketWithServiceDomain>> =
-        dataUseCase.getTicketByStatus(customerId, ticketStatus).asLiveData()
+    fun getTicketByStatus(customerId: Int) = dataUseCase.getTicketsWaiting(customerId).asLiveData()
+    fun getTicketByHistory(customerId: Int) = dataUseCase.getTicketsHistory(customerId).asLiveData()
 }
