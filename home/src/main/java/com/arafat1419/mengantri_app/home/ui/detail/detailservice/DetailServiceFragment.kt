2d280b2ce1a1226ecc8162851692cedf849fe143
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-07-31 15:33:13 +0800

    add bottom message to create ticket

--- home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailservice/DetailServiceFragment.kt
+++ home/src/main/java/com/arafat1419/mengantri_app/home/ui/detail/detailservice/DetailServiceFragment.kt
@@ -2,7 +2,6 @@ package com.arafat1419.mengantri_app.home.ui.detail.detailservice
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -162,7 +161,7 @@ class DetailServiceFragment : Fragment() {
                     serviceCountDomain?.service?.serviceId!!,
                     customerSessionManager.fetchCustomerName()!!,
                     customerSessionManager.fetchCustomerPhone()!!,
-                    "-",
+                    notes,
                     ticketDate,
                     estimatedTime.estimatedTime!!,
                 ).observe(viewLifecycleOwner) { ticket ->
