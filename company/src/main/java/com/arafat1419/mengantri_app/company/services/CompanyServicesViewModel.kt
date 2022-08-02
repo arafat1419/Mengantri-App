df462440d7c261b602ad84e853671d12c8aa77ca
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-08-02 19:40:47 +0800

    add endpoint for companyServicesViewModel

--- company/src/main/java/com/arafat1419/mengantri_app/company/services/CompanyServicesViewModel.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/services/CompanyServicesViewModel.kt
@@ -4,34 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.arafat1419.mengantri_app.core.domain.model.ServiceDomain
-import com.arafat1419.mengantri_app.core.domain.model.ServiceOnlyDomain
 import com.arafat1419.mengantri_app.core.domain.usecase.DataUseCase
 
 class CompanyServicesViewModel(private val dataUseCase: DataUseCase) : ViewModel() {
     fun getServices(companyId: Int): LiveData<List<ServiceDomain>> =
-        dataUseCase.getServices(companyId).asLiveData()
+        dataUseCase.getServicesByCompany(companyId).asLiveData()
 
-    fun postService(serviceOnlyDomain: ServiceOnlyDomain): LiveData<ServiceOnlyDomain> =
-        dataUseCase.postService(serviceOnlyDomain).asLiveData()
+    fun postService(serviceDomain: ServiceDomain): LiveData<ServiceDomain> =
+        dataUseCase.postService(serviceDomain).asLiveData()
 
-    fun updateService(
-        serviceId: Int,
-        serviceName: String,
-        serviceOpenTime: String,
-        serviceCloseTime: String,
-        serviceAnnouncement: String,
-        serviceMaxCustomer: Int,
-        serviceStatus: Int,
-        serviceDay: List<String>
-    ): LiveData<ServiceOnlyDomain> =
-        dataUseCase.updateService(
-            serviceId,
-            serviceName,
-            serviceOpenTime,
-            serviceCloseTime,
-            serviceAnnouncement,
-            serviceMaxCustomer,
-            serviceStatus,
-            serviceDay
-        ).asLiveData()
+    fun updateService(serviceId: Int, serviceDomain: ServiceDomain): LiveData<ServiceDomain> =
+        dataUseCase.updateService(serviceId, serviceDomain).asLiveData()
+
+    fun deleteService(serviceId: Int): LiveData<Boolean> =
+        dataUseCase.deleteService(serviceId).asLiveData()
 }
