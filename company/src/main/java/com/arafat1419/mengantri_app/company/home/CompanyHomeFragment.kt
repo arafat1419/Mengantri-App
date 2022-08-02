c365c541f97aeee18e16b5ef9a491ac94091e9f4
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-08-02 14:27:18 +0800

    re-implement companyHome

--- company/src/main/java/com/arafat1419/mengantri_app/company/home/CompanyHomeFragment.kt
+++ company/src/main/java/com/arafat1419/mengantri_app/company/home/CompanyHomeFragment.kt
@@ -11,8 +11,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.arafat1419.mengantri_app.company.R
 import com.arafat1419.mengantri_app.company.databinding.FragmentCompanyHomeBinding
 import com.arafat1419.mengantri_app.company.di.companyModule
-import com.arafat1419.mengantri_app.core.domain.model.ServiceCountDomain
-import com.arafat1419.mengantri_app.core.ui.AdapterCallback
 import com.arafat1419.mengantri_app.core.ui.adapter.ServicesAdapter
 import com.arafat1419.mengantri_app.core.utils.CompanySessionManager
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -22,26 +20,30 @@ import org.koin.core.context.loadKoinModules
 
 @ExperimentalCoroutinesApi
 @FlowPreview
-class CompanyHomeFragment : Fragment(), AdapterCallback<ServiceCountDomain> {
+class CompanyHomeFragment : Fragment() {
 
     // Initilize binding with null because we need to set it null again when fragment destroy
     private var _binding: FragmentCompanyHomeBinding? = null
-    private val binding get() = _binding
+    private val binding get() = _binding!!
 
-    private lateinit var companySessionManager: CompanySessionManager
-
-    private var navHostFragment: Fragment? = null
+    private val navHostFragment: Fragment? by lazy { parentFragmentManager.findFragmentById(R.id.fragment_container) }
 
     // Initialize viewModel with koin
     private val viewModel: CompanyHomeViewModel by viewModel()
 
+    private val servicesAdapter by lazy { ServicesAdapter() }
+
+    private val companySessionManager: CompanySessionManager by lazy {
+        CompanySessionManager(requireContext())
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentCompanyHomeBinding.inflate(layoutInflater, container, false)
-        return binding?.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -51,42 +53,36 @@ class CompanyHomeFragment : Fragment(), AdapterCallback<ServiceCountDomain> {
         loadKoinModules(companyModule)
 
         setRecyclerView()
+        getCompanyService()
+        onItemClicked()
+    }
 
-        // Initialize nav host fragment as fragment container
-        navHostFragment =
-            parentFragmentManager.findFragmentById(R.id.company_fragment_container)
-
-        companySessionManager = CompanySessionManager(requireContext())
-
+    private fun getCompanyService() {
         viewModel.getServiceAndServed(companySessionManager.fetchCompanyId())
-            .observe(viewLifecycleOwner) {
-                binding?.rvCompanyServices?.adapter.let { adapter ->
-                    when (adapter) {
-                        is ServicesAdapter -> {
-                            adapter.setData(it)
-                            adapter.notifyDataSetChanged()
-                        }
-                    }
+            .observe(viewLifecycleOwner) { listServiceCount ->
+                if (listServiceCount != null) {
+                    servicesAdapter.setData(listServiceCount)
                 }
             }
     }
 
-    override fun onItemClicked(data: ServiceCountDomain) {
-        val bundle = bundleOf(
-            CompanyCustomersFragment.EXTRA_SERVICE_ID to data.services.serviceId,
-            CompanyCustomersFragment.EXTRA_SERVICE_NAME to data.services.serviceName
-        )
-        navHostFragment?.findNavController()?.navigate(
-            R.id.action_companyHomeFragment_to_companyCustomersFragment,
-            bundle
-        )
+    private fun onItemClicked() {
+        servicesAdapter.onItemClicked = {
+            val bundle = bundleOf(
+                CompanyCustomersFragment.EXTRA_SERVICE_ID to it.service?.serviceId
+            )
+            navHostFragment?.findNavController()?.navigate(
+                R.id.action_companyHomeFragment_to_companyCustomersFragment,
+                bundle
+            )
+        }
     }
 
     // Set recycler view with grid and use companies adapter as adapter
     private fun setRecyclerView() {
-        binding?.rvCompanyServices?.apply {
+        binding.rvCompanyServices.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-            adapter = ServicesAdapter(this@CompanyHomeFragment)
+            adapter = servicesAdapter
         }
     }
 
